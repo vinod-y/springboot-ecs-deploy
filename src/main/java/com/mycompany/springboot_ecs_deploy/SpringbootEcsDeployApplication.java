428f0b7195e2a579e1977d6063c53c5ba60e94eb
vinod y <yvinod.in@gmail.com>
2025-07-23 14:29:02 +0530

    first commit

---
+++ src/main/java/com/mycompany/springboot_ecs_deploy/SpringbootEcsDeployApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.springboot_ecs_deploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEcsDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEcsDeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/springboot_ecs_deploy/SpringbootEcsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.springboot_ecs_deploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
